dec76c926e95233ecd7dc8c186bee4018b2c201b
rominesmikeemiezl-svg <rominesmikeemiezl@gmail.com>
2025-12-23 20:44:26 +0800

    remove cors setup

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-    output: "standalone",
-     async headers() {
+  output: "standalone",
+  async headers() {
     return [
-      {
-        source: "/api/:path*",   // match all /api/* routes
-        headers: [
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE, OPTIONS" },
-          { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
-        ],
-      },
+      // {
+      //   source: "/api/:path*",   // match all /api/* routes
+      //   headers: [
+      //     { key: "Access-Control-Allow-Origin", value: "*" },
+      //     { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE, OPTIONS" },
+      //     { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
+      //   ],
+      // },
     ];
   },
 };
